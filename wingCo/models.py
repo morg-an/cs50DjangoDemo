47f4e9fbd65bee74f14427757f396f6e6c22412e
Morgan Schumann <91375960+morg-an@users.noreply.github.com>
2023-01-23 21:31:41 -0600

    add string methods to models.py

--- wingCo/models.py
+++ wingCo/models.py
@@ -8,10 +8,21 @@ class Flight(models.Model):
     destination = models.CharField(max_length=64)
     duration = models.SmallIntegerField()
 
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination}"
+
 class Passenger(models.Model):
     first = models.CharField(max_length=64)
     last = models.CharField(max_length=64)
 
+    def __str__(self):
+        return f"{self.id}: {self.first} {self.last}"
+
 class Airport(models.Model):
     city = models.CharField(max_length=64)
     airportCode = models.CharField(max_length=3)
+
+    def __str__(self):
+        return f"{self.id}: {self.city} ({self.airportCode})"
+
+# in db.sqlite3, these tables are stored as wingCo_airport, wingCo_flight, and wingCo_passenger
